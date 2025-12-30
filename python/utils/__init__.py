21c39bdbb31629b98a4208d8c47d0b500b6a7d3e
Ahmad Yousuf <ahmadyousuf2603@gmail.com>
2025-12-30 09:14:24 +0600

    Update __init__.py

--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -1,13 +1,13 @@
-from .vpn_detect import is_vpn_connected
+from .vpn_detect import VPNDetector
 from .bin_loader import get_platform_binaries
 from .is_online import is_online, check_connection
 from .tunnel import tunnel_process, tunnel_running, tunnel_url, start_tunnel, stop_tunnel, get_status
 
 __all__ = [
+    "VPNDetector",
     "get_platform_binaries",
     "is_online",
     "check_connection",
-    "is_vpn_connected",
     "tunnel_process",
     "tunnel_running",
     "tunnel_url",
